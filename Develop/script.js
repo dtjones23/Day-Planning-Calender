997db59a3ae5bcfa604dfce160b906423f9f23e4
DeAngelo Jones <deangelojones0708@gmail.com>
2023-10-11 21:01:21 -0500

    inserted remaining time blocks in HTML

--- Develop/script.js
+++ Develop/script.js
@@ -2,13 +2,18 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-// shows the current date
+// creates the current date
 var today = dayjs();
 var currentDay = dayjs().format('[Today\'s\ date is]'+ today);
+// displays current date
 $('#currentDay').text(today);
 
 
+
+
+
 $(function () {
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
