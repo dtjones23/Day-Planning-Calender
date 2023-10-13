9dd69404e22f1a62ad0ec681fdc18000684e2f82
DeAngelo Jones <deangelojones0708@gmail.com>
2023-10-13 17:55:02 -0500

    added local storge and function to display the user values

--- Develop/script.js
+++ Develop/script.js
@@ -1,38 +1,84 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-// creates the current date
-function updatecurrentTime() {
+// wrapped code in Jquery
+$(function () {
   const today = dayjs()
-  const formattedTime = today.format('HH:mm:ss')
+  const saveBtn = $('button')
+  // creates the current date
+  function updatecurrentTime() {
+    const today = dayjs()
+    const formattedTime = today.format('h:mm:ss A');
 
-  // displays running time in header
-  $('#current-time').text(formattedTime)
-    
-}  
-// calls updatecurrentTime when page loads
-$(document).ready(function() {
-  updatecurrentTime();
+    // running time 
+    $('#current-time').html(formattedTime)
+      
+  }  
+  // calls updatecurrentTime when page loads
+  $(document).ready(function() {
+    updatecurrentTime();
+    updateColors();
+
+  // update current time every second
+  setInterval(updatecurrentTime,1000);
+  setInterval(updateColors,60000)
+  
+  });
+
+
+  function updateColors() {
+  // change colors to match time of day in accordance to it being past, present or future 
+  
+    const currentHour= dayjs().hour();
+    // compares the hour of the day
+
+    resetColor();
+
+    // runs through each time block to assign the designated color
+    $('.time-block').each(function() {
+      if ($(this).attr('#id') < currentHour) {
+        $(this).children('.description').addClass('past');
+      } else if ($(this) == currentHour) {
+        $(this).children('.description').addClass('present');
+      } else {
+        $(this).children('.description').addClass('future');
+      }
+    })
+  };
+
+  // prevent overlapping of classes
+  function resetColor() {
+  $('.time-block').each(function(){
+    $(this).removeClass('past','present','future');
+  })
+  }
+
+  $('button').on('click', function(event){
+    event.preventDefault()
+  })
 });
-// update current time every second
-setInterval(updatecurrentTime,1000);
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page
+// data to input in local storage when save button is clicked
+var userData = JSON.parse(localStorage.getItem('userData')) || [
+  {id: "", text: ""},
+  {id: "", text: ""},
+  {id: "", text: ""},
+  {id: "", text: ""},
+  {id: "", text: ""},
+  {id: "", text: ""},
+  {id: "", text: ""},
+  {id: "", text: ""},
+  {id: "", text: ""},
+];
+
+
+function loadData (){
+  // displays the value
+  console.log(userData);
+
+  $('.time-block').each(function() {
+    var thisID = $(this).attr('id');
+    var thisText = $(this).children('.description').val();
+    console.log(thisID + '//' + thisText);
+
+    // text value displays userData.text
+    $(this).children('.description').val(userData[thisID -9].text)
+  })
+}
