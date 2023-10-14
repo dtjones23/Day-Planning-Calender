7c7c3a78019bd18fd1236c6a9a59b2b39cdda1a0
DeAngelo Jones <deangelojones0708@gmail.com>
2023-10-14 16:28:15 -0500

    updated function for update colors

--- Develop/script.js
+++ Develop/script.js
@@ -1,41 +1,36 @@
 // wrapped code in Jquery
 $(function () {
   const today = dayjs()
-  const saveBtn = $('button')
   // creates the current date
-  function updatecurrentTime() {
+  function updateCurrentTime() {
     const today = dayjs()
     const formattedTime = today.format('h:mm:ss A');
+    const currentDay = today.format('dddd, MMMM D');
 
     // running time 
     $('#current-time').html(formattedTime)
+    $('#current-day').html(currentDay)
+
       
   }  
   // calls updatecurrentTime when page loads
-  $(document).ready(function() {
-    updatecurrentTime();
+    updateCurrentTime();
     updateColors();
-
-  // update current time every second
-  setInterval(updatecurrentTime,1000);
-  setInterval(updateColors,60000)
-  
-  });
-
 
   function updateColors() {
   // change colors to match time of day in accordance to it being past, present or future 
   
-    const currentHour= dayjs().hour();
+    var currentHour= dayjs().hour();
+    console.log(currentHour);
     // compares the hour of the day
 
     resetColor();
 
     // runs through each time block to assign the designated color
     $('.time-block').each(function() {
-      if ($(this).attr('#id') < currentHour) {
+      if ($(this).attr('id') < currentHour) {
         $(this).children('.description').addClass('past');
-      } else if ($(this) == currentHour) {
+      } else if ($(this).attr('id') == currentHour) {
         $(this).children('.description').addClass('present');
       } else {
         $(this).children('.description').addClass('future');
@@ -53,32 +48,39 @@ $(function () {
   $('button').on('click', function(event){
     event.preventDefault()
   })
-});
-
-// data to input in local storage when save button is clicked
-var userData = JSON.parse(localStorage.getItem('userData')) || [
-  {id: "", text: ""},
-  {id: "", text: ""},
-  {id: "", text: ""},
-  {id: "", text: ""},
-  {id: "", text: ""},
-  {id: "", text: ""},
-  {id: "", text: ""},
-  {id: "", text: ""},
-  {id: "", text: ""},
-];
 
 
-function loadData (){
-  // displays the value
-  console.log(userData);
+  // data to input in local storage when save button is clicked
+  var userData = JSON.parse(localStorage.getItem('userData')) || [
+    {id: "", text: ""},
+    {id: "", text: ""},
+    {id: "", text: ""},
+    {id: "", text: ""},
+    {id: "", text: ""},
+    {id: "", text: ""},
+    {id: "", text: ""},
+    {id: "", text: ""},
+    {id: "", text: ""},
+  ];
 
-  $('.time-block').each(function() {
-    var thisID = $(this).attr('id');
-    var thisText = $(this).children('.description').val();
-    console.log(thisID + '//' + thisText);
 
-    // text value displays userData.text
-    $(this).children('.description').val(userData[thisID -9].text)
-  })
-}
+  // function loadData (){
+  //   // displays the value
+  //   console.log(userData);
+
+  //   $('.time-block').each(function() {
+  //     var thisID = $(this).attr('id');
+  //     var thisText = $(this).children('.description').val();
+  //     console.log(thisID + '//' + thisText);
+
+  //     // text value displays userData.text
+  //     $(this).children('.description').val(userData[thisID -9].text)
+  //   })
+  // }
+
+  // loadData()
+
+ setInterval(updateCurrentTime,1000);
+  setInterval(updateColors,60000)  
+});  
+
